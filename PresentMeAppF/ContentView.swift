f04d90de40d620038e5aa609b07755d11b292f10
Farah Alamri <ms.falamri42@gmail.com>
2023-10-08 12:36:47 +0300

    farah's present me app

--- PresentMeAppF/ContentView.swift
+++ PresentMeAppF/ContentView.swift
@@ -9,14 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            VStack(){
+                Image("FlorasLogo")
+                    .resizable(resizingMode: .stretch)
+                    .frame(width: /*@START_MENU_TOKEN@*/280.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/280.0/*@END_MENU_TOKEN@*/)
+                
+                    .clipShape(Circle())
+                    .overlay{
+                        Circle().stroke(.yellow, lineWidth: 10)
+                    }.position(x: 180, y: 250)
+                
+                VStack(alignment: .leading){
+                    Text("Farah Alamri")
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(.yellow)
+                    
+                    
+                    Text("Interested in design and arts mostly.")
+                        .font(.body)
+                        .fontWeight(.medium)
+                        .foregroundColor(.white)
+                    
+                }.position(x: 180, y:90)
+                    
+            }
+            .padding()
+            
         }
-        .padding()
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
