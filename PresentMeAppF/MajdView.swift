d1fb0f574fe23b42e7a84aa67ec99bf03b49e917
Majd <Majd.fahadd@gmail.com>
2023-10-08 12:57:56 +0300

    majd

---
+++ PresentMeAppF/MajdView.swift
@@ -0,0 +1,29 @@
+
+import SwiftUI
+
+struct MajdView: View {
+    var body: some View {
+        VStack {
+            Image("K")
+                .resizable()
+                .frame (width: 100,height: 100 )
+                .imageScale(.small)
+                .clipShape(Circle())
+                .foregroundColor(.brown)
+            
+            Text("Majd Almutairi")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.brown)
+                .foregroundColor(.black)
+            Text(" I am a cookies and brownies lover ")
+                .foregroundColor(.black)
+                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MajdView()
+}
