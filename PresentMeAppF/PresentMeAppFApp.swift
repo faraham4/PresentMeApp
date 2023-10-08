1f70ae17c9f9ce46e5ff9e7f27adcb284c7ef8ca
Farah Alamri <ms.falamri42@gmail.com>
2023-10-08 11:10:38 +0300

    Initial Commit

---
+++ PresentMeAppF/PresentMeAppFApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMeAppFApp.swift
+//  PresentMeAppF
+//
+//  Created by Farah Alamri on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PresentMeAppFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
